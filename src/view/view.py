52cc8759404d31c76f689d24d5485c6a6e7036e7
rhensen <mail@raimund-hensen.de>
2023-06-13 23:33:47 +0200

    Improved tabs with extra extendible class

--- src/view/view.py
+++ src/view/view.py
@@ -3,20 +3,99 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.scrolled import ScrolledFrame
+from tkfontawesome import icon_to_image as fontawesome
 from PIL import Image, ImageTk
+import os
 
 # from view.components.scrollable_frame import VerticalScrolledFrame
 from view.components.accordion import Accordion
 from view.widgets.object_types import ObjectTypeWidget
+from view.components.tab import Tabs, Tab
 # from controller.controller import *
 
 WINDOW_TITLE = "Object-centric Business App"
-MAXIMIZED = False
+if os.getlogin() == "RH":
+    MAXIMIZED = True
+else:
+    MAXIMIZED = False
 SIDEBAR_WIDTH_RATIO = 0.2
 SIDEBAR_MIN_WIDTH = 150
 TOOLBAR_HEIGHT = 40
 
 logger = logging.getLogger("app_logger")
+
+
+def view():
+    return View.instance
+
+
+class SidebarTab(Tab):
+    def __init__(self, master, title, **kwargs):
+        super().__init__(master=master, title=title, **kwargs)
+        # Main layout
+        self.rowconfigure(0, minsize=TOOLBAR_HEIGHT)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, minsize=SIDEBAR_MIN_WIDTH, weight=1)
+        self.columnconfigure(1, minsize=SIDEBAR_MIN_WIDTH / SIDEBAR_WIDTH_RATIO,
+                             weight=int(round(1 / SIDEBAR_WIDTH_RATIO, 0)) - 1)
+
+        view().style.configure("sidebar.TFrame", background="#e0e0e0")
+        # self.sidebar = VerticalScrolledFrame(master=self.window, style="sidebar.TFrame")
+        self.sidebar = ScrolledFrame(master=self)
+        self.sidebar.grid(row=1, column=0, sticky=NSEW)
+        self.main = tk.Frame(master=self)
+        self.main.grid(row=1, column=1, sticky=NSEW)
+        self.interior = self.main
+
+
+class FilterTab(SidebarTab):
+    def __init__(self, master):
+        super().__init__(master=master, title="Filters and Settings")
+
+        # # Create test button to demonstrate MVC event propagation
+        # self.test_label = tk.Label(master=self.main, text="---")
+        # self.test_btn = tk.Button(master=self.main, text="MVC Test", command=self.controller.test_action)
+        # self.test_label.pack()
+        # self.test_btn.pack()
+
+        # Sidebar contents
+        acc = Accordion(self.sidebar, title_height=50, bootstyle=SECONDARY)
+        # Object types
+        self.ot_container = acc.add_chord(title='Object types', expanded=True)
+        self.ot_widget = None
+        # Activities
+        self.act_container = acc.add_chord(title='Activities')
+        self.act_widget = None
+        tk.Label(self.act_container, text='hello world', bg='white').pack()
+
+        acc.pack(side=TOP, fill=X)
+
+    def on_open(self):
+       pass
+
+
+class PetriNetTab(Tab):
+    def __init__(self, master):
+        super().__init__(master=master, title="Petri Net")
+
+        # Petri Net Discovery
+        # self.pn_button = tk.Button(master=self, text="Discover Petri Net", command=self.display_petri_net)
+        # self.pn_button.pack()
+
+    def on_open(self):
+        self.display_petri_net()
+
+    def display_petri_net(self):
+        # TODO move to controller, with callback
+        # TODO display in window
+        logger.info("Discovering petri net")
+        view().controller.model.ocel.discover_petri_net()
+        ocpn_image = Image.open('static/img/ocpn.png')
+        image_tk = ImageTk.PhotoImage(ocpn_image)
+        # label = ttk.Label(self.window, text="Petri Net", image=image_tk)
+        # label.grid(row=2, column=0, sticky='nsew')
+        # label.pack()
+
 
 class Window(tk.Tk):
     def __init__(self):
@@ -27,7 +106,10 @@ class Window(tk.Tk):
 
 
 class View:
+    instance = None
+
     def __init__(self, controller, theme):
+        View.instance = self
         self.controller = controller
         self.window = Window()
 
@@ -40,36 +122,14 @@ class View:
         self.toolbar.pack(side=TOP, fill=X)
 
         # Tabs
-        self.tabs = {}
-        self.tab_widget = ttk.Notebook(master=self.window)
+        self.tab_widget = Tabs(master=self.window)
         self.tab_widget.pack(side=TOP, fill=BOTH)
 
         # create a new frame
-        tab1 = self.add_tab(key="filters", title="Filters and Settings")
-        tab2 = self.add_tab(key="petri_net", title="Petri Net")
-
-        tab1.rowconfigure(0, minsize=TOOLBAR_HEIGHT)
-        tab1.rowconfigure(1, weight=1)
-        tab1.columnconfigure(0, minsize=SIDEBAR_MIN_WIDTH, weight=1)
-        tab1.columnconfigure(1, minsize=SIDEBAR_MIN_WIDTH / SIDEBAR_WIDTH_RATIO,
-                             weight=int(round(1 / SIDEBAR_WIDTH_RATIO, 0)) - 1)
-
-        style.configure("sidebar.TFrame", background="#e0e0e0")
-        # self.sidebar = VerticalScrolledFrame(master=self.window, style="sidebar.TFrame")
-        self.sidebar = ScrolledFrame(master=tab1)
-        self.sidebar.grid(row=1, column=0, sticky=NSEW)
-        self.main = tk.Frame(master=tab1)
-        self.main.grid(row=1, column=1, sticky=NSEW)
-
-        # # Create test button to demonstrate MVC event propagation
-        # self.test_label = tk.Label(master=self.main, text="---")
-        # self.test_btn = tk.Button(master=self.main, text="MVC Test", command=self.controller.test_action)
-        # self.test_label.pack()
-        # self.test_btn.pack()
-
-        # Petri Net Discovery
-        self.pn_button = tk.Button(master=tab2, text="Discover Petri Net", command=self.display_petri_net)
-        self.pn_button.pack()
+        self.tab1 = FilterTab(self.tab_widget)
+        self.tab_widget.add_tab(self.tab1)
+        self.tab2 = PetriNetTab(self.tab_widget)
+        self.tab_widget.add_tab(self.tab2)
 
         # Toolbar contents
         ttk.Label(master=self.toolbar, text="[Toolbar]", bootstyle=DARK).pack(side=LEFT)
@@ -85,41 +145,9 @@ class View:
                                        command=lambda t=theme: self.change_theme(t))
         theme_menubutton["menu"] = theme_menu
 
-        # Sidebar contents
-        acc = Accordion(self.sidebar, title_height=50, bootstyle=SECONDARY)
-        # Object types
-        self.ot_container = acc.add_chord(title='Object types', expanded=True)
-        self.ot_widget = None
-        # Activities
-        self.act_container = acc.add_chord(title='Activities')
-        self.act_widget = None
-        tk.Label(self.act_container, text='hello world', bg='white').pack()
-
-        acc.pack(side=TOP, fill=X)
-
-    def add_tab(self, key, title, callback_open=None, callback_close=None):
-        self.tabs[key] = ttk.Frame(self.tab_widget)
-        self.tab_widget.add(self.tabs[key], text=title)
-        return self.tabs[key]
-
-    def on_open_tab_petri_net(self):
-        # TODO create Tab class, automatically calling event listener on opening
-        logger.info("Opened tab 'petri net'")
-
-    def display_petri_net(self):
-        # TODO move to controller, with callback
-        # TODO display in window
-        logger.info("Discovering petri net")
-        self.controller.model.ocel.discover_petri_net()
-        ocpn_image = Image.open('static/img/ocpn.png')
-        image_tk = ImageTk.PhotoImage(ocpn_image)
-        # label = ttk.Label(self.window, text="Petri Net", image=image_tk)
-        # label.grid(row=2, column=0, sticky='nsew')
-        # label.pack()
-
     def init_object_types(self, object_types, counts, model, colors=None):
-        self.ot_widget = ObjectTypeWidget(self.ot_container, object_types, counts, model, colors)
-        self.ot_widget.pack(fill=X)
+        self.tab1.ot_widget = ObjectTypeWidget(self.tab1.ot_container, object_types, counts, model, colors)
+        self.tab1.ot_widget.pack(fill=X)
 
     def init_activities(self, activities):
         pass
